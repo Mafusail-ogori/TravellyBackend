0b1983b4963b80b147ea380135db975869a3da8c
Dan Prots <76164438+Mafusail-ogori@users.noreply.github.com>
2023-09-14 12:08:07 +0300

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'Hajaomija123' ,
+    password: '*******' ,
     host: 'localhost',
     port: 5432,
     database: 'Travelly'
